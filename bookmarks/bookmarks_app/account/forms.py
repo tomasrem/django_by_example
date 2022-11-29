62cc185072c5dc9b0e1c34d8ff8b6f3ca56f420e
Tomáš Remiš <remiss.tomass@gmail.com>
2022-11-29 21:31:31 +0100

    Adding form validation for the email and making email unique for each profile.

--- bookmarks/bookmarks_app/account/forms.py
+++ bookmarks/bookmarks_app/account/forms.py
@@ -16,6 +16,7 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ['username' , 'first_name', 'email']
 
+    #password validation
     def clean_password2(self): # you can provide clean_<field name>(): method to any of your form field and this method is called when we call the is_valid() method
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
@@ -23,11 +24,30 @@ class UserRegistrationForm(forms.ModelForm):
         return cd['password2']
 
 
+    #email validation
+    def clean_email(self):
+        data = self.cleaned_data['email'] # get the email from submited form 
+        if User.objects.filter(email=data).exists(): # check if the user exist with given email 
+            raise forms.ValidationError('Email already in use , please try another one or restore password for your account.') # if yes raise validation error
+        return data # otherwise return the value of the form 
+
+
 # form for editing user info
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name' , 'last_name' , 'email']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+
+        if qs.exists():
+            if data == '':
+                raise forms.ValidationError(' Please enter your new email adress ')
+            else:
+                raise forms.ValidationError(' Email already in use. ')
+        return data
 
 # form for editing profile info 
 class ProfileEditForm(forms.ModelForm):

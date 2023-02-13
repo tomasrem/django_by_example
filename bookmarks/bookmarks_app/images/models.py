066aeb164f69e485ce8465e47efe31ad67a18ae2
Tomáš Remiš <remiss.tomass@gmail.com>
2023-02-13 21:28:39 +0100

    First chages after a loong time :) Hope this time itll work out

--- bookmarks/bookmarks_app/images/models.py
+++ bookmarks/bookmarks_app/images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
 
@@ -31,3 +32,6 @@ class Image(models.Model):
             self.slug = slugify(self.title)
 
         super().save(*args , **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail' , args=[self.id , self.slug])

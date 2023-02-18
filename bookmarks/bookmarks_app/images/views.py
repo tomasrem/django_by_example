d6776f1500a708b59b521b98fa55635e0b8d6cd4
Tomáš Remiš <remiss.tomass@gmail.com>
2023-02-18 23:58:51 +0100

    Adding liking systen to the page and ajax requests.

--- bookmarks/bookmarks_app/images/views.py
+++ bookmarks/bookmarks_app/images/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 @login_required
@@ -34,3 +35,25 @@ def image_detail(request , id , slug ):
     image = get_object_or_404(Image , id = id , slug = slug)
     
     return render(request, 'images/image/detail.html' , {'section' :'images' , 'image' : image})
+
+#image liking 
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id = image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            
+            return JsonResponse({'status' : 'ok'})
+
+        except Image.DoesNotExist:
+            pass
+
+    return JsonResponse({'status' : 'error'})
+
